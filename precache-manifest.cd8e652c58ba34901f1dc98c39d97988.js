391137a230cb873b26300c5751ec785e799d0af1
MALIHEH MOHAMMADI NAVASATLI <35292708+malihe6900@users.noreply.github.com>
2020-09-05 01:00:42 +0200

    Updates

---
+++ precache-manifest.cd8e652c58ba34901f1dc98c39d97988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a66424d3911bad13c113",
+    "url": "/Maktabkhone-todo1/static/css/main.ec650135.chunk.css"
+  },
+  {
+    "revision": "a66424d3911bad13c113",
+    "url": "/Maktabkhone-todo1/static/js/main.a66424d3.chunk.js"
+  },
+  {
+    "revision": "5a145c9e4462c3dddbfd",
+    "url": "/Maktabkhone-todo1/static/js/1.5a145c9e.chunk.js"
+  },
+  {
+    "revision": "85bbc0d3733805bb772c",
+    "url": "/Maktabkhone-todo1/static/js/runtime~main.85bbc0d3.js"
+  },
+  {
+    "revision": "3a937a5f115ec73c215f3cc4420faa27",
+    "url": "/Maktabkhone-todo1/index.html"
+  }
+];
